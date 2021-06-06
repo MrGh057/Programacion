b4c2511ca35e4f92ae64edca6078fd094fd7db34
MrGh057 <82605006+MrGh057@users.noreply.github.com>
2021-06-05 23:51:11 -0500

    Add files via upload

---
+++ Python/00_Hola_Mundo/HelloWorld.py
@@ -0,0 +1,5 @@
+#Para comenzar veremos la sintaxis básica de un script sencillo que lo único que hará será 
+#imprimir la famosa frase Hello World en pantalla.
+
+print("Hello World")
+#Felicidades has creado tu primer script :D
